d4e96be1144f87eda2845d1ca1e7db39147f9da4
sainz1407 <80474991+sainz1407@users.noreply.github.com>
2024-02-14 12:12:13 +0700

    Update UOAdl-gui.py

--- UOAdl-gui.py
+++ UOAdl-gui.py
@@ -1,8 +1,11 @@
 import os
 import json
+import threading
+import io
+import zlib
 from Crypto.Cipher import AES
 import base64
-import zlib
+from PIL import Image, ImageTk
 import httpx
 import UnityPy
 import tkinter as tk
@@ -13,7 +16,7 @@ class UniOnAirDownloader:
     def __init__(self, root):
         self.root = root
         self.root.title("UNI'S ON AIR Image Downloader")
-        self.root.geometry("500x200")
+        self.root.geometry("530x450")
 
         self.label_resver = ttk.Label(root, text="Server Date (resver):")
         self.label_resver.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
@@ -33,11 +36,17 @@ class UniOnAirDownloader:
         self.combobox_members = ttk.Combobox(root, width=40)
         self.combobox_members.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
 
-        self.button_download = ttk.Button(root, text="Download", command=self.download_images)
+        self.button_download = ttk.Button(root, text="Download", command=self.start_download_thread)
         self.button_download.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
         self.progress_bar = ttk.Progressbar(root, orient=tk.HORIZONTAL, length=400, mode='determinate')
         self.progress_bar.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+
+        self.canvas = tk.Canvas(root, width=200, height=200)
+        self.canvas.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+
+    def start_download_thread(self):
+        threading.Thread(target=self.download_images).start()
 
     def load_member_data(self):
         with open("member.data.json", "r", encoding="utf-8") as member_file:
@@ -55,6 +64,21 @@ class UniOnAirDownloader:
         self.combobox_groups['values'] = groups
         self.combobox_members['values'] = members
 
+    def update_canvas_image(self, img_data):
+        if img_data:
+            img = Image.open(io.BytesIO(img_data))
+            max_width = 400
+            width, height = img.size
+            new_height = int((max_width / width) * height)
+            img = img.resize((max_width, new_height), Image.LANCZOS)
+            img_tk = ImageTk.PhotoImage(img)
+            self.canvas.config(width=img_tk.width(), height=img_tk.height())
+            self.canvas.create_image(0, 0, anchor=tk.NW, image=img_tk)
+            self.canvas.image = img_tk
+            self.canvas.update()
+        else:
+            self.canvas.delete("all")
+
     def download_images(self):
         resver = self.entry_resver.get() or None
         selected_group = self.combobox_groups.get()
@@ -66,7 +90,12 @@ class UniOnAirDownloader:
             selected_member = [selected_unisonair]
         self.progress_bar['value'] = 0
         self.root.update_idletasks()
-        main(resver, selected_group, selected_member, self.progress_bar)
+        self.update_canvas_image(b"")  # Clear canvas before downloading new images
+
+        total_assets = 0  # Total number of assets to download
+        assets_downloaded = 0  # Number of assets already downloaded
+        main(resver, selected_group, selected_member, self.progress_bar, self.update_canvas_image, total_assets, assets_downloaded)
+
         messagebox.showinfo("Download Complete", "Downloaded successfully.")
 
 def decrypt(file_path, key, iv, output_path):
@@ -95,9 +124,9 @@ def download_file(url, destination):
             for data in response.iter_bytes():
                 downloaded_size += len(data)
                 file.write(data)
-                progress = min(int((downloaded_size / total_size) * 100), 100)
-                app.progress_bar['value'] = progress
-                app.root.update_idletasks()
+                # progress = min(int((downloaded_size / total_size) * 100), 100)
+                # app.progress_bar['value'] = progress
+                # app.root.update_idletasks()
     return True
 
 def load_json(file_path):
@@ -105,7 +134,7 @@ def load_json(file_path):
         json_data = json.load(file)
     return json_data["assets_masters"]
 
-def main(resver, groups, members, progress_bar):
+def main(resver, groups, members, progress_bar, update_canvas_image, total_assets, assets_downloaded):
     assets_masters_url = f"https://cdn-masters.unis-on-air.com/production/{resver}/Android/assets_masters"
     file_path = ".temp/assets_masters"
     output_file = f"{file_path}.json"
@@ -144,11 +173,17 @@ def main(resver, groups, members, progress_bar):
                         if not os.path.exists(member_folder):
                             os.makedirs(member_folder)
                         if f"content/cards/scene_card_{selected_member}_" in item["code"]:
-                            file_url = base_url + item["code"]
-                            destination = os.path.join(".temp", item["code"].split("/")[-1])
                             assets_path = os.path.join(member_folder, f"{item['code'].split('/')[-1].replace('.unity3d', '.png')}")
-                            if not os.path.exists(assets_path):
+                            if os.path.exists(assets_path):
+                                print(f"File already exists: {assets_path}")
+                                assets_downloaded += 1
+                                progress_bar["value"] = (assets_downloaded / total_assets) * 100
+                                progress_bar.update()
+                                continue
+                            else:
                                 print(f"Downloading: {assets_path}")
+                                file_url = base_url + item["code"]
+                                destination = os.path.join(".temp", item["code"].split("/")[-1])
                                 if download_file(file_url, destination):
                                     env = UnityPy.load(destination)
                                     texture_size = 0
@@ -162,12 +197,16 @@ def main(resver, groups, members, progress_bar):
                                                 assets = tex
                                     if assets is not None:
                                         assets.image.save(assets_path)
+                                        with open(assets_path, "rb") as img_file:
+                                            img_data = img_file.read()
+                                        update_canvas_image(img_data)  # Update canvas with downloaded image
                                     os.remove(destination)
                                     assets_downloaded += 1
                                     progress_bar["value"] = (assets_downloaded / total_assets) * 100
                                     progress_bar.update()
-                            else:
-                                print(f"File already exists: {assets_path}")
+                                else:
+                                    print(f"Failed to download: {assets_path}")
+                                    update_canvas_image(b"")
     if os.path.exists(file_path):
         os.remove(file_path)
     if os.path.exists(output_file):
